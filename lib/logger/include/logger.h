4f659544667182e5b9637b6790b61ae9269d0a90
avihai_adler <avihai.adler@gmail.com>
2022-07-31 13:38:50 +0300

    struct logger has changed. logger_init now returns a struct logger *

--- lib/logger/include/logger.h
+++ lib/logger/include/logger.h
@@ -1,23 +1,25 @@
 #pragma once
 #include <stdbool.h>
 
+struct logger;
+
 /* a naive logger implementation for a multithreaded program. the logger must be
  * initialized before attempting to use it in a multithreaded programs with
  * log_init. file_name may be NULL, in such case
  * - the logger will output to stdout */
 
-enum level { ERROR, WARNING, DEBUG, INFO };
+enum level { ERROR, WARN, DEBUG, INFO };
 
 /* initializes the logger 'object'. must be called before any use of the logger.
  * expects a valid file_name. if the file doesn't exists - the logger will
  * create one for you. file_name may be NULL - in such case the logger will
  * output to stdout. returns true on success, false otherwise */
-bool log_init(char *file_name);
+struct logger *logger_init(char *file_name);
 
-void log_msg(enum level level, char *msg);
+void log_msg(struct logger *logger, enum level level, char *msg);
 
-/* destroys the logger 'object'. must be called after all threads are joined(or
- * killed). any attempt to call the function while the threads still uses the
- * logger - may result in undefined behavior. return true on success, false
- * otherwise */
-bool log_destroy(void);
+/* destroys the logger 'object'. must be called after all threads are
+ * joined(or killed). any attempt to call the function while the threads
+ * still uses the logger - may result in undefined behavior. return true on
+ * success, false otherwise */
+void logger_destroy(struct logger *logger);
