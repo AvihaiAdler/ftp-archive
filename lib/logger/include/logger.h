d76983f768c9508cff7a614c5fe1bd1ffda2d126
avihai_adler <avihai.adler@gmail.com>
2022-07-31 14:20:54 +0300

    some comments fixes

--- lib/logger/include/logger.h
+++ lib/logger/include/logger.h
@@ -13,13 +13,13 @@ enum level { ERROR, WARN, DEBUG, INFO };
 /* initializes the logger 'object'. must be called before any use of the logger.
  * expects a valid file_name. if the file doesn't exists - the logger will
  * create one for you. file_name may be NULL - in such case the logger will
- * output to stdout. returns true on success, false otherwise */
+ * output to stdout. returns a pointer to a logger object on success, NULL
+ * otherwise */
 struct logger *logger_init(char *file_name);
 
 void log_msg(struct logger *logger, enum level level, char *msg);
 
 /* destroys the logger 'object'. must be called after all threads are
  * joined(or killed). any attempt to call the function while the threads
- * still uses the logger - may result in undefined behavior. return true on
- * success, false otherwise */
+ * still uses the logger - may result in undefined behavior */
 void logger_destroy(struct logger *logger);
