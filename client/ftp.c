9b1a7f70470b3816cffe78212ce6c45fe2189ff5
avihai_adler <avihai.adler@gmail.com>
2022-08-10 20:04:29 +0300

    minor changes

--- client/ftp.c
+++ client/ftp.c
@@ -1,11 +1,18 @@
+#include <arpa/inet.h>
 #include <netdb.h>
+#include <stdint.h>
 #include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
 #include <sys/socket.h>
 #include <sys/types.h>
+#include <unistd.h>
 
+#include "include/payload.h"
 #include "include/util.h"
 #include "logger.h"
+
+#define INPUT_SIZE UINT8_MAX
 
 int main(int argc, char *argv[]) {
   struct logger *logger = logger_init(NULL);
@@ -25,7 +32,7 @@ int main(int argc, char *argv[]) {
 
   struct addrinfo *addr = get_addr_info(ip, port);
   if (!addr) {
-    char *msg = get_error_msg(ip, port);
+    char *msg = get_msg(ip, port, FAILURE);
     if (msg) log_msg(logger, ERROR, msg);
     free(msg);
 
@@ -37,7 +44,7 @@ int main(int argc, char *argv[]) {
   freeaddrinfo(addr);
 
   if (sockfd == -1) {
-    char *msg = get_error_msg(ip, port);
+    char *msg = get_msg(ip, port, FAILURE);
     if (msg) log_msg(logger, ERROR, msg);
     free(msg);
 
@@ -46,6 +53,31 @@ int main(int argc, char *argv[]) {
   }
 
   // all connected at this point
+  char *msg = get_msg(ip, port, SUCCESS);
+  log_msg(logger, INFO, msg);
+  free(msg);
+
+  bool abort = false;
+  uint8_t input[INPUT_SIZE];
+  do {
+    if (!get_input(input, sizeof input)) continue;
+    input[strcspn(input, "\n")] = 0;  // strip the trailing '\n'
+
+    /*
+    TODO
+      check input command, decide whether you should recv afterwards
+    */
+
+    // if (!send_payload(sockfd,
+    //                   (struct payload){.size = strlen(input), .data =
+    //                   input})) {
+
+    //   continue;
+    // }
+
+    // struct payload ret = recv_payload(sockfd);
+
+  } while (!abort);
 
   close(sockfd);
   return 0;
