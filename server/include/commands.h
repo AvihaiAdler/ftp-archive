c65bd4709d845df077e00545e099821788bb80b3
avihai_adler <avihai.adler@gmail.com>
2022-08-19 16:29:32 +0300

    commands hashes

---
+++ server/include/commands.h
@@ -0,0 +1,11 @@
+#pragma once
+
+// magic numbers are the hashes of these strings taken from the hash commands in hash_table.c
+enum command_hash {
+  QUIT = 0x7c9d0608,
+  RETR = 0x7c9d4fc2,
+  STOR = 0x7c9e1b4d,
+  APPE = 0x7c942b8b,
+  DELE = 0x7c95a15f,
+  LIST = 0x7c9a1661
+};
