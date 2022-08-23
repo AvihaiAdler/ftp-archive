c095310c87f2c5d1875300d3d7db3f14225ad75c
avihai_adler <avihai.adler@gmail.com>
2022-08-23 20:42:40 +0300

    payloads are now broken down to request, reply and data block

--- server/include/payload.h
+++ server/include/payload.h
@@ -1,6 +1,11 @@
 #pragma once
 
 #include <stdint.h>
+#include <sys/types.h>
+
+#define REQUEST_MAX_LEN 64
+#define REPLY_MAX_LEN 256
+#define DATA_BLOCK_MAX_LEN 2048
 
 enum reply_codes {
   CMD_OK = 200,
@@ -16,16 +21,24 @@ enum reply_codes {
 
 struct reply {
   uint16_t code;
-  uint64_t length;
-  const uint8_t *data;
+  uint16_t length;
+  int8_t reply[REPLY_MAX_LEN];
 };
 
 struct request {
-  uint64_t length;
-  uint8_t *data;
+  uint16_t length;
+  int8_t request[REQUEST_MAX_LEN];
 };
 
-void send_payload(struct reply reply, int sockfd, int flags);
+struct data_block {
+  uint16_t length;
+  int8_t data[DATA_BLOCK_MAX_LEN];
+};
 
-/* returns a struct request who's its data has been heap allocated. must be free'd */
-struct request recieve_payload(int sockfd, int flags);
+ssize_t send_reply(struct reply reply, int sockfd, int flags);
+
+struct request recieve_request(int sockfd, int flags);
+
+ssize_t send_data(struct data_block data, int sockfd, int flags);
+
+struct data_block receive_data(int sockfd, int flags);
--- server/src/payload.c
+++ server/src/payload.c
@@ -15,53 +15,80 @@ static uint16_t change_order_u16(uint16_t num) {
   return is_big_endian() ? num : bswap_16(num);
 }
 
-static uint64_t change_order_u64(uint64_t num) {
-  return is_big_endian() ? num : bswap_64(num);
-}
+ssize_t send_reply(struct reply reply, int sockfd, int flags) {
+  if (sockfd < 0) return -1;
+  if (!reply.code) return -1;
+  if (reply.length > REPLY_MAX_LEN - 1) return -1;
 
-void send_payload(struct reply payload, int sockfd, int flags) {
-  if (!payload.code && !payload.length) return;
-
-  payload.code = change_order_u16(payload.code);
-  payload.length = change_order_u64(payload.length);
+  reply.code = change_order_u16(reply.code);
+  uint16_t reply_length = change_order_u16(reply.length);
 
   // send reply code
-  ssize_t bytes_sent = send(sockfd, &payload.code, sizeof payload.code, flags);
-  if (bytes_sent == -1) return;  // error.
+  ssize_t bytes_sent = send(sockfd, &reply.code, sizeof reply.code, flags);
+  if (bytes_sent == -1) return -1;  // error.
 
   // send data length
-  while (send(sockfd, &payload.length, sizeof payload.length, flags) == -1) {
-    continue;
-  }
-
-  if (!payload.data) return;
+  bytes_sent = send(sockfd, &reply_length, sizeof reply_length, flags);
+  if (bytes_sent == -1) return -1;
 
   ssize_t ret = 0;
-  for (uint64_t sent = 0; sent < payload.length; sent += ret) {
-    ret = send(sockfd, payload.data, payload.length, flags);
-    if (ret == -1) return;  // error. possibly sockfd was closed
+  for (uint16_t sent = 0; sent < reply.length; sent += ret) {
+    ret = send(sockfd, reply.reply + sent, reply.length - sent, flags);
+    if (ret == -1) return -1;  // error. possibly sockfd was closed
   }
+  return 0;
 }
 
-struct request recieve_payload(int sockfd, int flags) {
-  if (sockfd == -1) return (struct request){0};
+struct request recieve_request(int sockfd, int flags) {
+  if (sockfd < 0) return (struct request){0};
 
   struct request request = {0};
   ssize_t recieved = recv(sockfd, &request.length, sizeof request.length, flags);
   if (recieved == -1) return (struct request){0};
 
-  request.length = change_order_u64(request.length);
-
-  request.data = calloc(request.length + 1, 1);
-  if (!request.data) return (struct request){0};
+  request.length = change_order_u16(request.length);
+  if (request.length > REQUEST_MAX_LEN - 1) return (struct request){0};
 
   ssize_t ret = 0;
-  for (uint64_t recved = 0; recved < request.length; recved += ret) {
-    ret = recv(sockfd, request.data + recved, request.length - recved, flags);
-    if (ret == -1) {
-      free(request.data);
-      return (struct request){0};
-    }
+  for (uint16_t recved = 0; recved < request.length; recved += ret) {
+    ret = recv(sockfd, request.request + recved, request.length - recved, flags);
+    if (ret == -1) return (struct request){0};  // error. possibly sockfd was closed
   }
   return request;
+}
+
+ssize_t send_data(struct data_block data, int sockfd, int flags) {
+  if (sockfd < 0) return -1;
+  if (!data.length) return -1;
+  if (data.length > DATA_BLOCK_MAX_LEN) return -1;
+
+  uint16_t data_length = change_order_u16(data.length);
+  ssize_t bytes_sent = send(sockfd, &data_length, sizeof data_length, flags);
+  if (bytes_sent == -1) return -1;
+
+  ssize_t ret = 0;
+  for (uint16_t sent = 0; sent < data.length; sent += ret) {
+    ret = send(sockfd, data.data + sent, data.length - sent, flags);
+    if (ret == -1) return -1;
+  }
+  return 0;
+}
+
+struct data_block receive_data(int sockfd, int flags) {
+  if (sockfd < 0) return (struct data_block){0};
+
+  struct data_block data = {0};
+
+  ssize_t recved = recv(sockfd, &data.length, sizeof data.length, flags);
+  if (recved == -1) return (struct data_block){0};
+
+  data.length = change_order_u16(data.length);
+  if (data.length > DATA_BLOCK_MAX_LEN) return (struct data_block){0};
+
+  ssize_t ret = 0;
+  for (uint16_t recieved = 0; recieved < data.length; recieved += ret) {
+    ret = recv(sockfd, data.data + recieved, data.length - recieved, flags);
+    if (ret == -1) return (struct data_block){0};
+  }
+  return data;
 }
