570782e0ff67baf527c52c32dde61aac03b74904
avihai_adler <avihai.adler@gmail.com>
2022-08-25 21:04:52 +0300

    removed all handlers to a different TU. removed port_args. minor changes to the args which passed into the threads

--- server/include/util.h
+++ server/include/util.h
@@ -1,7 +1,6 @@
 #pragma once
 
 #include <netdb.h>
-#include <poll.h>
 #include <stddef.h>
 #include "hash_table.h"
 #include "include/payload.h"
@@ -12,26 +11,21 @@
 #include "vector_s.h"
 
 struct regular_args {
-  struct request request;
-  struct session *remote;
-};
-
-struct port_args {
   int remote_fd;
-  struct session *local;  // may never chagned by the threads
+  struct session local;
   struct vector_s *sessions;
   struct request request;
 };
 
 struct request_args {
   int remote_fd;
-  struct session *local;  // may never chagned by the threads
+  struct session local;
   struct vector_s *sessions;
   struct thread_pool *thread_pool;
 };
 
 struct args_wrapper {
-  enum type { REGULAR, SET_PORT, HANDLE_REQUEST } type;
+  enum type { REGULAR, REQUEST } type;
   void *args;
   struct logger *logger;
 };
@@ -41,6 +35,8 @@ void cleanup(struct hash_table *properties,
              struct thread_pool *thread_pool,
              struct vector_s *sessions,
              struct vector *pollfds);
+
+struct addrinfo *get_addr_info(const char *host, const char *serv, int flags);
 
 /* get a socket. if flags == AI_PASSIVE the socket will listen() for incoming connections */
 int get_socket(struct logger *logger, const char *host, const char *serv, int conn_q_size, int flags);
@@ -60,19 +56,7 @@ int cmpr_sessions(const void *a, const void *b);
 /* removes a pair of control_sockfd, data_sockfd and closes both sockfds */
 void close_session(struct vector_s *sessions, struct logger *logger, int fd);
 
-char *tolower_str(char *str, size_t len);
-
-/* creates a new handle_request() task for the thread_pool to handle. used by the main thread */
-void add_request_task(struct session *local,
-                      int remote_fd,
-                      struct vector_s *sessions,
-                      struct thread_pool *thread_pool,
-                      struct logger *logger);
-
-/* parse a request and creates a new task for the thread_pool to handle */
-int handle_request(void *arg);
-
 void destroy_task(void *task);
 
 /* gets the ip and port associated with a sockfd */
-void get_ip_and_port(int sockfd, char *host, size_t host_len, char *serv, size_t serv_len);
+void get_ip_and_port(int sockfd, char *ip, size_t ip_size, char *port, size_t port_size);
