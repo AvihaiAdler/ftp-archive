1642357b64e4038b4276f105a475375ffad3c8fa
avihai_adler <avihai.adler@gmail.com>
2022-09-13 17:23:45 +0300

    major changes accross the board: - get_socket() role is taken by get_server_socket() & get_client_socket(). - the cmpr function for the sessions nowconsider a session's data_fd as well. - added get_local_ip(), to get the local ip of the server.

--- server/include/util.h
+++ server/include/util.h
@@ -1,6 +1,7 @@
 #pragma once
 
 #include <netdb.h>
+#include <stdbool.h>
 #include <stddef.h>
 #include "hash_table.h"
 #include "include/payload.h"
@@ -9,26 +10,6 @@
 #include "thread_pool.h"
 #include "vector.h"
 #include "vector_s.h"
-
-struct regular_args {
-  int remote_fd;
-  struct session local;
-  struct vector_s *sessions;
-  struct request request;
-};
-
-struct request_args {
-  int remote_fd;
-  struct session local;
-  struct vector_s *sessions;
-  struct thread_pool *thread_pool;
-};
-
-struct args_wrapper {
-  enum type { REGULAR, REQUEST } type;
-  void *args;
-  struct logger *logger;
-};
 
 void cleanup(struct hash_table *properties,
              struct logger *logger,
@@ -39,7 +20,9 @@ void cleanup(struct hash_table *properties,
 struct addrinfo *get_addr_info(const char *host, const char *serv, int flags);
 
 /* get a socket. if flags == AI_PASSIVE the socket will listen() for incoming connections */
-int get_socket(struct logger *logger, const char *host, const char *serv, int conn_q_size, int flags);
+int get_server_socket(struct logger *logger, const char *host, const char *serv, int conn_q_size, int flags);
+
+int get_client_socket(struct logger *logger, const char *host, const char *serv, int flags);
 
 /* adds a sockfd to the vector of sockfd used by poll() */
 void add_fd(struct vector *pollfds, struct logger *logger, int fd, int events);
@@ -48,15 +31,18 @@ void add_fd(struct vector *pollfds, struct logger *logger, int fd, int events);
 void add_session(struct vector_s *sessions, struct logger *logger, struct session *session);
 
 /* removes a sockfd to the vector of sockfd used by poll() */
-void remove_fd(struct vector *pollfds, struct logger *logger, int fd);
+void remove_fd(struct vector *pollfds, int fd);
 
 /* compr between control_sockfd, data_sockfd pair. used to initialize the vector of these pairs */
 int cmpr_sessions(const void *a, const void *b);
 
 /* removes a pair of control_sockfd, data_sockfd and closes both sockfds */
-void close_session(struct vector_s *sessions, struct logger *logger, int fd);
+void close_session(struct vector_s *sessions, int fd);
 
 void destroy_task(void *task);
 
 /* gets the ip and port associated with a sockfd */
 void get_ip_and_port(int sockfd, char *ip, size_t ip_size, char *port, size_t port_size);
+
+/* get the (first listed) local ip of the machine */
+bool get_local_ip(char *ip, size_t ip_size, int inet);
--- server/src/util.c
+++ server/src/util.c
@@ -1,7 +1,9 @@
 #define _POSIX_C_SOURCE 200112L
 #define _GNU_SOURCE
 #include "include/util.h"
+#include <arpa/inet.h>
 #include <fcntl.h>
+#include <ifaddrs.h>
 #include <netdb.h>
 #include <poll.h>
 #include <stdint.h>
@@ -34,11 +36,7 @@ void cleanup(struct hash_table *properties,
 
 void destroy_task(void *task) {
   struct task *t = task;
-  if (t->args) {
-    struct args_wrapper *args_wrapper = t->args;
-    free(args_wrapper->args);
-    free(t->args);
-  }
+  if (t->args) { free(t->args); }
 }
 
 struct addrinfo *get_addr_info(const char *host, const char *serv, int flags) {
@@ -52,12 +50,16 @@ struct addrinfo *get_addr_info(const char *host, const char *serv, int flags) {
   return info;
 }
 
-int get_socket(struct logger *logger, const char *host, const char *serv, int conn_q_size, int flags) {
+int get_server_socket(struct logger *logger, const char *host, const char *serv, int conn_q_size, int flags) {
   if (!host && !serv) return -1;
 
   struct addrinfo *info = get_addr_info(host, serv, flags);
   if (!info) {
-    logger_log(logger, ERROR, "[get_socket] no available addresses for %s:%s", host ? host : "", serv ? serv : "");
+    logger_log(logger,
+               ERROR,
+               "[get_server_socket] no available addresses for %s:%s",
+               host ? host : "",
+               serv ? serv : "");
     return -1;
   }
 
@@ -68,25 +70,82 @@ int get_socket(struct logger *logger, const char *host, const char *serv, int co
     sockfd = socket(available->ai_family, available->ai_socktype, available->ai_protocol);
     if (sockfd == -1) continue;
 
-    if (flags && fcntl(sockfd, F_SETFL, O_NONBLOCK) == -1) continue;
+    if (fcntl(sockfd, F_SETFL, O_NONBLOCK) == -1) continue;
 
-    int val = 1;
-    if (setsockopt(sockfd, SOL_SOCKET, SO_REUSEADDR, &val, sizeof val) == -1) continue;
+    // int val = 1;
+    // if (setsockopt(sockfd, SOL_SOCKET, SO_REUSEADDR, &val, sizeof val) == -1) continue;
 
     if (bind(sockfd, available->ai_addr, available->ai_addrlen) == -1) continue;
 
-    if (!flags) {
-      success = true;
-    } else if (flags && listen(sockfd, conn_q_size) == 0) {
-      success = true;
-    }
+    if (listen(sockfd, conn_q_size) == 0) { success = true; }
   }
 
-  // failed to listen()/bind()
   freeaddrinfo(info);
+
+  // failed to listen()/bind()
   if (!success) {
     close(sockfd);
-    logger_log(logger, ERROR, "[get_socket] couldn't get a socket for %s:%s", host ? host : "", serv ? serv : "");
+    logger_log(logger,
+               ERROR,
+               "[get_server_socket] couldn't get a socket for %s:%s",
+               host ? host : "",
+               serv ? serv : "");
+    return -1;
+  }
+
+  return sockfd;
+}
+
+int get_client_socket(struct logger *logger, const char *host, const char *serv, int flags) {
+  if (!host && !serv) return -1;
+
+  struct addrinfo *info = get_addr_info(host, serv, flags);
+  if (!info) {
+    logger_log(logger,
+               ERROR,
+               "[get_client_socket] no available addresses for %s:%s",
+               host ? host : "",
+               serv ? serv : "");
+    return -1;
+  }
+
+  char local_ip[INET6_ADDRSTRLEN] = {0};
+  struct sockaddr_storage local_addr = {0};
+  socklen_t local_addr_len = sizeof local_addr;
+  if (get_local_ip(local_ip, sizeof local_ip, AF_INET)) {
+    inet_pton(AF_INET, local_ip, &local_addr);
+  } else if (get_local_ip(local_ip, sizeof local_ip, AF_INET6)) {
+    inet_pton(AF_INET6, local_ip, &local_addr);
+  } else {
+    freeaddrinfo(info);
+    logger_log(logger, ERROR, "[get_client_socket] failed to fetch local ip");
+    return -1;
+  }
+
+  int sockfd = -1;
+  bool success = false;
+  for (struct addrinfo *available = info; available && !success; available = available->ai_next) {
+    sockfd = socket(available->ai_family, available->ai_socktype, available->ai_protocol);
+    if (sockfd == -1) continue;
+
+    // int val = 1;
+    // if (setsockopt(sockfd, SOL_SOCKET, SO_REUSEADDR, &val, sizeof val) == -1) continue;
+
+    if (bind(sockfd, (struct sockaddr *)&local_addr, local_addr_len) == -1) continue;
+
+    if (connect(sockfd, available->ai_addr, available->ai_addrlen) == 0) { success = true; }
+  }
+
+  freeaddrinfo(info);
+
+  // failed to listen()/bind()
+  if (!success) {
+    close(sockfd);
+    logger_log(logger,
+               ERROR,
+               "[get_client_socket] couldn't get a socket for %s:%s",
+               host ? host : "",
+               serv ? serv : "");
     return -1;
   }
 
@@ -127,18 +186,17 @@ int cmpr_sessions(const void *a, const void *b) {
   const struct session *s_a = a;
   const struct session *s_b = b;
 
-  return (s_a->control_fd > s_b->control_fd) - (s_a->control_fd < s_b->control_fd);
+  if (s_a->control_fd == s_b->control_fd || s_a->data_fd == s_b->data_fd) return 0;
+  if (s_a->control_fd > s_b->control_fd) return 1;
+  return -1;
 }
 
-void remove_fd(struct vector *pollfds, struct logger *logger, int fd) {
+void remove_fd(struct vector *pollfds, int fd) {
   if (!pollfds) return;
 
   long long pos = vector_index_of(pollfds, &(struct pollfd){.fd = fd}, cmpr_pfds);
 
-  if (pos == N_EXISTS) {
-    if (logger) logger_log(logger, INFO, "[remove_fd] fd [%d] dons't exist", fd);
-    return;
-  }
+  if (pos == N_EXISTS) return;
 
   struct pollfd *pfd = vector_remove_at(pollfds, pos);
   if (!pfd) return;
@@ -146,17 +204,10 @@ void remove_fd(struct vector *pollfds, struct logger *logger, int fd) {
   free(pfd);
 }
 
-void close_session(struct vector_s *sessions, struct logger *logger, int fd) {
+void close_session(struct vector_s *sessions, int fd) {
   if (!sessions) return;
 
-  long long pos = vector_s_index_of(sessions, &(struct session){.control_fd = fd});
-
-  if (pos == N_EXISTS) {
-    if (logger) logger_log(logger, INFO, "[close_session] the session with fd [%d] doesn't exists", fd);
-    return;
-  }
-
-  struct session *session = vector_s_remove_at(sessions, pos);
+  struct session *session = vector_s_remove(sessions, &(struct session){.control_fd = fd});
   if (!session) return;
 
   close(session->control_fd);
@@ -172,4 +223,38 @@ void get_ip_and_port(int sockfd, char *ip, size_t ip_size, char *port, size_t po
   if (getsockname(sockfd, (struct sockaddr *)&addr, &addrlen) == 0) {
     getnameinfo((struct sockaddr *)&addr, addrlen, ip, ip_size, port, port_size, NI_NUMERICHOST | NI_NUMERICSERV);
   }
+}
+
+bool get_local_ip(char *ip, size_t ip_size, int inet) {
+  if (ip_size < INET6_ADDRSTRLEN) return false;
+  if (inet != AF_INET && inet != AF_INET6) return false;
+
+  struct ifaddrs *ifaddrs = NULL;
+
+  getifaddrs(&ifaddrs);
+
+  for (struct ifaddrs *ifa = ifaddrs; ifa; ifa = ifa->ifa_next) {
+    if (!ifa->ifa_addr) continue;
+
+    if (ifa->ifa_addr->sa_family == inet) {  // IPv4
+      struct sockaddr_in addr = {0};
+      memcpy(&addr, ifa->ifa_addr, sizeof addr);
+
+      if (strcmp(ifa->ifa_name, "eth0") == 0) {
+        inet_ntop(AF_INET, &addr.sin_addr, ip, ip_size);
+        break;
+      }
+    } else if (ifa->ifa_addr->sa_family == inet) {  // IPv6
+      struct sockaddr_in6 addr = {0};
+      memcpy(&addr, ifa->ifa_addr, sizeof addr);
+
+      if (strcmp(ifa->ifa_name, "eth0") == 0) {
+        inet_ntop(AF_INET6, &addr.sin6_addr, ip, ip_size);
+        break;
+      }
+    }
+  }
+
+  if (ifaddrs) freeifaddrs(ifaddrs);
+  return true;
 }
