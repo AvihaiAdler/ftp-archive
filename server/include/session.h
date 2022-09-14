da1bcd7d23ecaca0aa69bf44c0a3af596ad6b27c
avihai_adler <avihai.adler@gmail.com>
2022-09-14 21:23:52 +0300

    added a context to the session

--- server/include/session.h
+++ server/include/session.h
@@ -1,7 +1,16 @@
 #pragma once
 
+#define FILE_NAME_LEN 128
+
 struct session {
   int control_fd;
   int data_fd;
   enum { PASSIVE, ACTIVE } data_sock_type;
+
+  struct context {
+    int passive_sockfd;
+    char *file_name;
+    char *curr_dir;
+    int (*handler)(void *);
+  } context;
 };
