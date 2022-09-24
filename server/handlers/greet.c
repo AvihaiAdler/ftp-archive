9b1e2047a1a4adcefc0ace5f84d7a53d537b6320
avihai_adler <avihai.adler@gmail.com>
2022-09-24 17:33:59 +0300

    greet no longer opens a data connection

--- server/handlers/greet.c
+++ server/handlers/greet.c
@@ -1,5 +1,6 @@
 #include "greet.h"
 #include <stdlib.h>
+#include <string.h>
 #include "misc/util.h"
 #include "util.h"
 
@@ -10,8 +11,8 @@ int greet(void *arg) {
   struct log_context log_context = {.func_name = "login"};
   get_ip_and_port(args->remote_fd, log_context.ip, sizeof log_context.ip, log_context.port, sizeof log_context.port);
 
-  struct session *session = vector_s_find(args->sessions, &(struct session){.fds.control_fd = args->remote_fd});
-  if (!session) {
+  struct session *tmp_session = vector_s_find(args->sessions, &(struct session){.fds.control_fd = args->remote_fd});
+  if (!tmp_session) {
     logger_log(args->logger,
                ERROR,
                "[%lu] [%s] [%s:%s] failed to find session [%d]",
@@ -28,8 +29,12 @@ int greet(void *arg) {
     return 1;
   }
 
-  session->context.curr_dir = calloc(MAX_PATH_LEN + 1, 1);
-  if (!session->context.curr_dir) {
+  struct session session = {0};
+  memcpy(&session, tmp_session, sizeof session);
+  free(tmp_session);
+
+  session.context.curr_dir = calloc(MAX_PATH_LEN + 1, 1);
+  if (!session.context.curr_dir) {
     logger_log(args->logger,
                ERROR,
                "[%lu] [%s] [%s:%s] failed allocate memory for session::context::curr_dir",
@@ -40,36 +45,12 @@ int greet(void *arg) {
     send_reply_wrapper(args->remote_fd,
                        args->logger,
                        RPLY_ACTION_INCOMPLETE_LCL_ERROR,
-                       "%d action incomplete",
+                       "%d action incomplete. internal error",
                        RPLY_ACTION_INCOMPLETE_LCL_ERROR);
     return 1;
   }
 
-  // open data_sockfd
-  int sockfd = open_data_connection(session, args->logger, &log_context);
-  if (sockfd == -1) {
-    logger_log(args->logger,
-               ERROR,
-               "[%lu] [%s] [%s:%s] failed to establish data connection",
-               thrd_current,
-               log_context.func_name,
-               log_context.ip,
-               log_context.port);
-    send_reply_wrapper(args->remote_fd,
-                       args->logger,
-                       RPLY_ACTION_INCOMPLETE_LCL_ERROR,
-                       "%d action incomplete",
-                       RPLY_ACTION_INCOMPLETE_LCL_ERROR);
-    return 1;
-  }
-
-  if (session->data_sock_type == ACTIVE) {
-    session->fds.data_fd = sockfd;
-  } else {  // should never happened
-    session->fds.listen_sockfd = sockfd;
-  }
-
-  bool ret = update_session(args->sessions, args->logger, session);
+  bool ret = update_session(args->sessions, args->logger, &session);
   if (!ret) {
     logger_log(args->logger,
                ERROR,
@@ -78,7 +59,7 @@ int greet(void *arg) {
                log_context.func_name,
                log_context.ip,
                log_context.port,
-               session->fds.control_fd);
+               session.fds.control_fd);
     return 1;
   }
 
@@ -89,8 +70,6 @@ int greet(void *arg) {
              log_context.func_name,
              log_context.ip,
              log_context.port);
-
-  free(session);
 
   send_reply_wrapper(args->remote_fd, args->logger, RPLY_CMD_OK, "%d ok. service ready", RPLY_CMD_OK);
   return 0;
--- server/handlers/greet.h
+++ server/handlers/greet.h
@@ -1,4 +1,4 @@
 #pragma once
 
-/* builds a session and open data connection. if all goes well return a "200 ok. service ready" to the client */
+/* builds a session. if all goes well return a "200 ok. service ready" to the client */
 int greet(void *arg);
