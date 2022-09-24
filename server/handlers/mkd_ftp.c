7f86e678eae00ccc893c9326799d25c601c5b668
avihai_adler <avihai.adler@gmail.com>
2022-09-24 20:09:46 +0300

    minor changes

--- server/handlers/mkd_ftp.c
+++ server/handlers/mkd_ftp.c
@@ -1,4 +1,5 @@
 #include "mkd_ftp.h"
+#include <errno.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -36,7 +37,8 @@ int make_directory(void *arg) {
 
   // get the desired directory path
   const char *new_dir_name = args->req_args.request_args;
-  if (!new_dir_name || *new_dir_name == '.') {  // no such path specified or dir name starts with a .
+  if (!validate_path(new_dir_name, args->logger, &context) || !strchr(new_dir_name, '/') ||
+      *new_dir_name == '.') {  // no such path specified or dir name starts with a .
     logger_log(args->logger,
                ERROR,
                "[%lu] [%s] [%s:%s] invalid request arguments",
@@ -44,15 +46,6 @@ int make_directory(void *arg) {
                context.func_name,
                context.ip,
                context.port);
-    send_reply_wrapper(args->remote_fd,
-                       args->logger,
-                       RPLY_ARGS_SYNTAX_ERR,
-                       "[%d] invalid arguments",
-                       RPLY_ARGS_SYNTAX_ERR);
-    return 1;
-  }
-
-  if (!validate_path(new_dir_name, args->logger, &context) || !strchr(new_dir_name, '/')) {
     send_reply_wrapper(args->remote_fd,
                        args->logger,
                        RPLY_ARGS_SYNTAX_ERR,
@@ -86,6 +79,14 @@ int make_directory(void *arg) {
   char path[MAX_PATH_LEN];
   snprintf(path, len + 1, "%s/%s", session.context.curr_dir, new_dir_name);
   if (mkdir(path, S_IRUSR | S_IWUSR | S_IXUSR) != 0) {
+    logger_log(args->logger,
+               ERROR,
+               "[%lu] [%s] [%s:%s] mkdir failed. reason [%s]",
+               thrd_current(),
+               context.func_name,
+               context.ip,
+               context.port,
+               strerr_safe(errno));
     send_reply_wrapper(args->remote_fd,
                        args->logger,
                        RPLY_ACTION_INCOMPLETE_LCL_ERROR,
