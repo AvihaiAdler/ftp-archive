b5f1bd7c7987da18b3348b1e503c72813f2a68b4
avihai_adler <avihai.adler@gmail.com>
2022-10-27 01:20:31 +0300

    cwd without any args will now set current directory to the root directory (similar to cd ~)

--- server/handlers/cwd_ftp.c
+++ server/handlers/cwd_ftp.c
@@ -33,77 +33,62 @@ int change_directory(void *arg) {
   memcpy(&session, tmp_session, sizeof session);
   free(tmp_session);
 
-  if (!validate_path(args->req_args.request_args, args->logger)) {
-    send_reply_wrapper(args->remote_fd,
-                       args->logger,
-                       RPLY_ARGS_SYNTAX_ERR,
-                       "[%d] invalid arguments",
-                       RPLY_ARGS_SYNTAX_ERR);
-    return 1;
+  if (!*args->req_args.request_args) {
+    *session.context.curr_dir = 0;
+  } else {
+    if (!validate_path(args->req_args.request_args, args->logger)) {
+      send_reply_wrapper(args->remote_fd,
+                         args->logger,
+                         RPLY_ARGS_SYNTAX_ERR,
+                         "[%d] invalid arguments",
+                         RPLY_ARGS_SYNTAX_ERR);
+      return 1;
+    }
+
+    // try to open the desired directory
+    int len = snprintf(NULL, 0, "%s/%s", session.context.root_dir, args->req_args.request_args);
+
+    // path is too long
+    if (len < 0 || len + 1 > MAX_PATH_LEN - 1) {
+      logger_log(args->logger,
+                 ERROR,
+                 "[%lu] [%s] [%s:%s] path too long",
+                 thrd_current(),
+                 __func__,
+                 session.context.ip,
+                 session.context.port);
+      send_reply_wrapper(args->remote_fd,
+                         args->logger,
+                         RPLY_ACTION_INCOMPLETE_LCL_ERROR,
+                         "[%d] action incomplete. internal process error (path too long)",
+                         RPLY_ACTION_INCOMPLETE_LCL_ERROR);
+      return 1;
+    }
+
+    char tmp_path[MAX_PATH_LEN] = {0};
+    snprintf(tmp_path, len + 1, "%s/%s", session.context.root_dir, args->req_args.request_args);
+    int ret = open(tmp_path, O_RDONLY | O_DIRECTORY);
+    if (ret == -1) {  // desired directory doesn't exist
+      logger_log(args->logger,
+                 ERROR,
+                 "[%lu] [%s] [%s:%s] invalid path [%s]",
+                 thrd_current(),
+                 __func__,
+                 session.context.ip,
+                 session.context.port,
+                 tmp_path);
+      send_reply_wrapper(args->remote_fd,
+                         args->logger,
+                         RPLY_ARGS_SYNTAX_ERR,
+                         "[%d] action incomplete. internal process error (invalid path)",
+                         RPLY_ARGS_SYNTAX_ERR);
+      return 1;
+    }
+    close(ret);
+
+    // replace session::context::curr_dir
+    strcpy(session.context.curr_dir, args->req_args.request_args);
   }
-
-  // try to open the desired directory
-  int len = snprintf(NULL, 0, "%s/%s", session.context.root_dir, args->req_args.request_args);
-
-  // path is too long
-  if (len < 0 || len + 1 > MAX_PATH_LEN - 1) {
-    logger_log(args->logger,
-               ERROR,
-               "[%lu] [%s] [%s:%s] path too long",
-               thrd_current(),
-               __func__,
-               session.context.ip,
-               session.context.port);
-    send_reply_wrapper(args->remote_fd,
-                       args->logger,
-                       RPLY_ACTION_INCOMPLETE_LCL_ERROR,
-                       "[%d] action incomplete. internal process error (path too long)",
-                       RPLY_ACTION_INCOMPLETE_LCL_ERROR);
-    return 1;
-  }
-
-  char tmp_path[MAX_PATH_LEN] = {0};
-  snprintf(tmp_path, len + 1, "%s/%s", session.context.root_dir, args->req_args.request_args);
-  int ret = open(tmp_path, O_RDONLY | O_DIRECTORY);
-  if (ret == -1) {  // desired directory doesn't exist
-    logger_log(args->logger,
-               ERROR,
-               "[%lu] [%s] [%s:%s] invalid path [%s]",
-               thrd_current(),
-               __func__,
-               session.context.ip,
-               session.context.port,
-               tmp_path);
-    send_reply_wrapper(args->remote_fd,
-                       args->logger,
-                       RPLY_ARGS_SYNTAX_ERR,
-                       "[%d] action incomplete. internal process error (invalid path)",
-                       RPLY_ARGS_SYNTAX_ERR);
-    return 1;
-  }
-  close(ret);
-
-  // path exceeds reply length
-  len = snprintf(NULL, 0, "[%d] ok. %s", RPLY_CMD_OK, args->req_args.request_args);
-  if (len + 1 > REPLY_MAX_LEN - 1) {
-    logger_log(args->logger,
-               ERROR,
-               "[%lu] [%s] [%s:%s] path exeeds reply length [%d]",
-               thrd_current(),
-               __func__,
-               session.context.ip,
-               session.context.port,
-               REPLY_MAX_LEN - 1);
-    send_reply_wrapper(args->remote_fd,
-                       args->logger,
-                       RPLY_ACTION_INCOMPLETE_LCL_ERROR,
-                       "[%d] action incomplete. internal process error",
-                       RPLY_ACTION_INCOMPLETE_LCL_ERROR);
-    return 1;
-  }
-
-  // replace session::context::curr_dir
-  strcpy(session.context.curr_dir, tmp_path + strlen(session.context.root_dir) + 1);
 
   // replace the session
   if (!update_session(args->sessions, args->logger, &session)) {
@@ -118,17 +103,18 @@ int change_directory(void *arg) {
   send_reply_wrapper(session.fds.control_fd,
                      args->logger,
                      RPLY_CMD_OK,
-                     "[%d] ok. %s/%s",
+                     "[%d] ok. [%s]",
                      RPLY_CMD_OK,
-                     session.context.curr_dir);
+                     *session.context.curr_dir ? session.context.curr_dir : "/");
   logger_log(args->logger,
              INFO,
-             "[%lu] [%s] [%s:%s] executed successfuly. working directory changed to [%s]",
+             "[%lu] [%s] [%s:%s] executed successfuly. working directory changed to [%s/%s]",
              thrd_current(),
              __func__,
              session.context.ip,
              session.context.port,
-             tmp_path);
+             session.context.root_dir,
+             session.context.curr_dir);
 
   return 0;
 }
